79d18caac4a9a0501985bca7a842e571b83116e0
jhoonsar@gmail.com <jhoonsar@gmail.com@6c2f81a4-5db7-099a-539b-8ad50865dc21>
2010-12-01 04:55:04 +0000

    Cambio de clases y actualización de dependencias de Spring

--- trunk/Proyecto_VENP/test/venp/web/actions/TestLocacionCerrarVotacion.java
+++ trunk/Proyecto_VENP/test/venp/web/actions/TestLocacionCerrarVotacion.java
@@ -7,11 +7,12 @@ import org.junit.Test;
 import venp.services.ProcesoElectoralService;
 
 public class TestLocacionCerrarVotacion {
-	ProcesoElectoralService servProc= new ProcesoElectoralService();
+	
 	
 	@Test
 	public void testCerrarVotacion() throws Exception {
 //		 = null;// = new ProcesoElectoralService();
+		ProcesoElectoralService servProc= new ProcesoElectoralService();
 		assertNotNull(servProc.estadoProceso());
 		assertSame(false, servProc.estadoProceso());
 	}
